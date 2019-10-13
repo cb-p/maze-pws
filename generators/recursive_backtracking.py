6c5a163716f62f7436b73d35714e6ab3feb8a826
Chris <chirssel@gmail.com>
2019-10-13 19:57:15 +0200

    Initial commit

---
+++ generators/recursive_backtracking.py
@@ -0,0 +1,54 @@
+import random
+from maze import *
+
+
+class RecursiveBacktrackingGenerator(MazeGenerator):
+    def __init__(self, maze):
+        super().__init__(maze)
+        self.stack = [(0, 0)]
+        self.visited = [(0, 0)]
+
+    def step(self):
+        current_pos = self.stack[len(self.stack) - 1]
+
+        self.maze.color(current_pos[0], current_pos[1], (255, 255, 255))
+        self.maze.highlight(current_pos[0], current_pos[1])
+
+        if len(self.visited) == self.maze.maze_width * self.maze.maze_height:
+            return
+
+        current_x, current_y = current_pos
+        directions = []
+        if current_pos[0] > 0 and (current_x - 1, current_y) not in self.visited:
+            directions.append(Direction.WEST)
+        if current_pos[0] < self.maze.maze_width - 1 and (current_x + 1, current_y) not in self.visited:
+            directions.append(Direction.EAST)
+        if current_pos[1] > 0 and (current_x, current_y - 1) not in self.visited:
+            directions.append(Direction.NORTH)
+        if current_pos[1] < self.maze.maze_height - 1 and (current_x, current_y + 1) not in self.visited:
+            directions.append(Direction.SOUTH)
+
+        if len(directions) == 0:
+            self.stack.pop(len(self.stack) - 1)
+            return
+
+        direction = random.choice(directions)
+        new_pos = current_pos
+        if direction == Direction.NORTH:
+            new_pos = (new_pos[0], new_pos[1] - 1)
+        elif direction == Direction.EAST:
+            new_pos = (new_pos[0] + 1, new_pos[1])
+        elif direction == Direction.SOUTH:
+            new_pos = (new_pos[0], new_pos[1] + 1)
+        elif direction == Direction.WEST:
+            new_pos = (new_pos[0] - 1, new_pos[1])
+
+        self.stack.append(new_pos)
+        self.visited.append(new_pos)
+
+        self.maze.connect_cell(current_pos[0], current_pos[1], direction)
+
+
+if __name__ == "__main__":
+    app = App(RecursiveBacktrackingGenerator)
+    app.start()
---
+++ maze.py
@@ -0,0 +1,179 @@
+import datetime
+import pygame
+
+
+class App:
+    def __init__(self, generator):
+        self.running = True
+        self.size = 640, 480
+
+        self.display_surface = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        pygame.display.set_caption('Maze')
+
+        self.frame_delta = datetime.timedelta(seconds=1/60)
+        self.step_delta = datetime.timedelta(seconds=1/60)
+        self.last_step_delta = datetime.timedelta()
+
+        pygame.font.init()
+        self.font = pygame.font.SysFont('Calibri', 18)
+
+        self.maze = Maze(10, 30, 20, 20)
+        self.maze_generator = generator(self.maze)
+
+        pygame.init()
+    
+    def update(self):
+        self.last_step_delta += self.frame_delta
+
+        while self.last_step_delta > self.step_delta:
+            self.last_step_delta -= self.step_delta
+            self.maze_generator.step()
+
+    def draw(self):
+        self.display_surface.fill((255, 255, 255))
+        self.maze.draw(self.display_surface)
+
+        delta_millis = self.frame_delta.days * 24.0 * 60.0 * 60.0 * 1000.0
+        delta_millis += self.frame_delta.seconds * 1000.0
+        delta_millis += self.frame_delta.microseconds / 1000.0
+        text = self.font.render('Maze - {} fps ({}ms)'.format(int(1000.0/delta_millis), int(delta_millis)), True, (0, 0, 0))
+        self.display_surface.blit(text, (10, 10))
+
+    def start(self):
+        last_frame_time = datetime.datetime.now()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.update()
+            self.draw()
+
+            now = datetime.datetime.now()
+            self.frame_delta = now - last_frame_time
+            last_frame_time = now
+
+            pygame.display.update()
+
+        pygame.quit()
+
+
+class Maze:
+    def __init__(self, x, y, maze_width, maze_height):
+        self.x = x
+        self.y = y
+        self.maze_width = maze_width
+        self.maze_height = maze_height
+
+        self.maze = [[MazeCell() for _ in range(0, self.maze_height)] for _ in range(0, self.maze_width)]
+        self.highlighted = (-1, -1)
+
+        self.tile_width = 12
+        self.border_width = 2
+
+    def full_width(self):
+        return self.maze_width * (self.tile_width + self.border_width) + self.border_width
+
+    def full_height(self):
+        return self.maze_height * (self.tile_width + self.border_width) + self.border_width
+    
+    def connect_cell(self, x, y, direction):
+        if direction == Direction.NORTH:
+            direction = Direction.opposite(direction)
+            y -= 1
+            
+        if direction == Direction.WEST:
+            direction = Direction.opposite(direction)
+            x -= 1
+
+        if x < 0 or y < 0 or x > self.maze_width - 1 or y > self.maze_height - 1:
+            return
+
+        if direction == Direction.SOUTH:
+            self.maze[x][y].connected_south = True
+            
+        if direction == Direction.EAST:
+            self.maze[x][y].connected_east = True
+    
+    def is_connected(self, x, y, direction):
+        if direction == Direction.NORTH:
+            direction = Direction.opposite(direction)
+            y -= 1
+            
+        if direction == Direction.WEST:
+            direction = Direction.opposite(direction)
+            x -= 1
+
+        if x < 0 or y < 0 or x > self.maze_width - 1 or y > self.maze_height - 1:
+            return False
+
+        if direction == Direction.SOUTH:
+            return self.maze[x][y].connected_south
+            
+        if direction == Direction.EAST:
+            return self.maze[x][y].connected_east
+    
+    def color(self, x, y, color):
+        if x < 0 or y < 0 or x > self.maze_width - 1 or y > self.maze_height - 1:
+            return
+        
+        self.maze[x][y].color = color
+
+    def highlight(self, x, y):
+        self.highlighted = (x, y)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (0, 0, 0), pygame.Rect(self.x, self.y, self.full_width(), self.full_height()))
+
+        for x in range(0, self.maze_width):
+            for y in range(0, self.maze_height):
+                cell = self.maze[x][y]
+
+                draw_x = self.x + x * (self.tile_width + self.border_width) + self.border_width
+                draw_y = self.y + y * (self.tile_width + self.border_width) + self.border_width
+
+                width_offset = self.border_width if cell.connected_east else 0
+                color = (0, 255, 0) if x == self.highlighted[0] and y == self.highlighted[1] else cell.color
+                pygame.draw.rect(surface, color, pygame.Rect(draw_x, draw_y, self.tile_width + width_offset, self.tile_width))
+
+                if cell.connected_south:
+                    pygame.draw.rect(surface, color, pygame.Rect(draw_x, draw_y + self.tile_width, self.tile_width, self.border_width))
+
+
+class MazeCell:
+    def __init__(self):
+        self.connected_east = False
+        self.connected_south = False
+        self.color = (200, 200, 200)
+
+
+class Direction:
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+    def opposite(direction):
+        if direction == Direction.NORTH:
+            return Direction.SOUTH
+        elif direction == Direction.EAST:
+            return Direction.WEST
+        elif direction == Direction.SOUTH:
+            return Direction.NORTH
+        else:
+            return Direction.EAST
+
+
+class MazeGenerator:
+    def __init__(self, maze):
+        self.maze = maze
+
+    def step(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("maze.py >> This file is meant to be used as a library, executing it will result in an useless simulation!")
+
+    app = App(MazeGenerator)
+    app.start()
