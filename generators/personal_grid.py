1af8e7d2383c27f72635d7f8f84ab948fe63fc6c
Louis <hooiberg12@gmail.com>
2019-10-17 11:13:34 +0200

    Added layout Personal Grid

---
+++ generators/personal_grid.py
@@ -0,0 +1,12 @@
+from maze import *
+
+
+class PersonalGridGenerator(MazeGenerator):
+    def __init__(self, maze):
+        super().__init__(self, maze)
+
+    def step(self):
+        pass
+
+    def update(self):
+        pass
